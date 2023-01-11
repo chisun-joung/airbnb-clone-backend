15f75a6a09a581181395598d381dc902c7d72cb3
chisun joung <csjoung@gmail.com>
2023-01-11 15:57:40 +0900

    search_fileds

--- houses/admin.py
+++ houses/admin.py
@@ -4,14 +4,17 @@ from .models import House
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
     """House admin"""
-    list_display = [
+    list_display = (
         'name', 
         'price_per_night', 
         'address',
         'pets_allowed'
-        ]
-    list_filter = [
+    )
+    list_filter = (
         'price_per_night',
         'pets_allowed'
-        ]
+    )
+    search_fields = (
+        'address',
+    )
     
