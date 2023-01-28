428f5b647e195c1d42f53aa3fd0c9e20cba89ead
chisun joung <csjoung@gmail.com>
2023-01-28 16:53:12 +0900

    urls parameters

--- rooms/views.py
+++ rooms/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def say_hello(request):
+def see_all_rooms(request):
     return HttpResponse("Hello World")
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"Hello World {room_id}")
