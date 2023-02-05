f79b5067970ae1e9b11d7d91d0cc2dca66370299
chisun joung <csjoung@gmail.com>
2023-02-05 15:48:18 +0900

    11.4 Room Detail

--- rooms/views.py
+++ rooms/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Amenity, Room
-from .serializer import AmenitySerializer, RoomsSerializer
+from .serializer import AmenitySerializer, RoomsListSerializer, RoomDetailSerializer
 
 
 class Amenities(APIView):
@@ -62,8 +62,21 @@ class AmenityDetail(APIView):
 class Rooms(APIView):
     def get(self, request):
         all_rooms = Room.objects.all()
-        serializer = RoomsSerializer(
+        serializer = RoomsListSerializer(
             all_rooms,
             many=True,
         )
+        return Response(serializer.data)
+
+
+class RoomDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        serializer = RoomDetailSerializer(room)
         return Response(serializer.data)
