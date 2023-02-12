41145931d64df8c99b30d8993f680df82753e1ea
chisun joung <csjoung@gmail.com>
2023-02-12 13:45:38 +0900

    11.16 Upload Photo

--- rooms/views.py
+++ rooms/views.py
@@ -13,6 +13,7 @@ from .serializers import AmenitySerializer, RoomsListSerializer, RoomDetailSeria
 from reviews.serializers import ReviewSerializer
 from categories.models import Category
 from django.conf import settings
+from medias.serializers import PhotoSerializer
 
 
 class Amenities(APIView):
@@ -228,5 +229,28 @@ class RoomAmenities(APIView):
 
 
 class RoomPhotos(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
     def post(self, request, pk):
-        pass
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                with transaction.atomic():
+                    photo = serializer.save(room=room)
+                    serializer = PhotoSerializer(photo)
+                    return Response(
+                        serializer.data,
+                    )
+            except:
+                raise ParseError("Can't create photo")
+        else:
+            return Response(serializer.errors)
