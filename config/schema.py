4ed2ea21dd90e1b3be4f54eaa7dc4f0c0fe9ca2d
chisun joung <csjoung@gmail.com>
2023-02-26 16:53:08 +0900

    14.4 dummy Query Mutation

--- config/schema.py
+++ config/schema.py
@@ -1,11 +1,46 @@
 import strawberry
+import typing
+
+
+@strawberry.type
+class Movie:
+    pk: int
+    title: str
+    year: int
+    rating: int
+
+
+movies_db = [
+    Movie(pk=1, title="Inception", year=2010, rating=8),
+]
+
+
+def movies():
+    return movies_db
+
+
+def movie(pk: int):
+    return movies_db[pk - 1]
+
+
+def addMovie(title: str, year: int, rating: int):
+    movie = Movie(pk=len(movies_db) + 1, title=title, year=year, rating=rating)
+    movies_db.append(movie)
+    return movie
 
 
 @strawberry.type
 class Query:
-    @strawberry.field
-    def ping(self) -> str:
-        return "pong"
+    movies: typing.List[Movie] = strawberry.field(resolver=movies)
+    movie: Movie = strawberry.field(resolver=movie)
 
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+    addMovie: Movie = strawberry.mutation(resolver=addMovie)
+
+
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+)
