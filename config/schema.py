c901f826a5da7452833a1c9087228052cad0051a
chisun joung <csjoung@gmail.com>
2023-02-26 21:24:36 +0900

    14.5 django type

--- config/schema.py
+++ config/schema.py
@@ -1,46 +1,18 @@
 import strawberry
-import typing
+from rooms import schema as rooms_schema
 
 
 @strawberry.type
-class Movie:
-    pk: int
-    title: str
-    year: int
-    rating: int
-
-
-movies_db = [
-    Movie(pk=1, title="Inception", year=2010, rating=8),
-]
-
-
-def movies():
-    return movies_db
-
-
-def movie(pk: int):
-    return movies_db[pk - 1]
-
-
-def addMovie(title: str, year: int, rating: int):
-    movie = Movie(pk=len(movies_db) + 1, title=title, year=year, rating=rating)
-    movies_db.append(movie)
-    return movie
-
-
-@strawberry.type
-class Query:
-    movies: typing.List[Movie] = strawberry.field(resolver=movies)
-    movie: Movie = strawberry.field(resolver=movie)
+class Query(rooms_schema.Query):
+    pass
 
 
 @strawberry.type
 class Mutation:
-    addMovie: Movie = strawberry.mutation(resolver=addMovie)
+    pass
 
 
 schema = strawberry.Schema(
     query=Query,
-    mutation=Mutation,
+    # mutation=Mutation,
 )
