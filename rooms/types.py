1ed882860b79922bebb1f6ae69fc09985ede1c28
chisun joung <csjoung@gmail.com>
2023-02-27 11:19:13 +0900

    14.7 Review Pagenation

--- rooms/types.py
+++ rooms/types.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 import strawberry
 from strawberry import auto
+import typing
 from . import models
+from users.types import UserType
+from reviews.types import ReviewType
 
 
 @strawberry.django.type(models.Room)
@@ -8,3 +12,15 @@ class RoomType:
     id: auto
     name: auto
     kind: auto
+    owner: "UserType"
+
+    @strawberry.field
+    def reviews(self, page: int) -> typing.List[ReviewType]:
+        page_size = settings.PAGE_SIZE
+        start = page_size * (page - 1)
+        end = start + page_size
+        return self.reviews.all()[start:end]
+
+    @strawberry.field
+    def rating(self) -> str:
+        return self.rating()
